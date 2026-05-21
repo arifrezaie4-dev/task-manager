515fc02fed025dccab002b62cb2ad8e1d95a2b7d
Arif Rezaie <arifrezaie4@gmail.com>
2026-05-21 19:12:57 +0430

    controller working with sample endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TasksModule } from 'src/tasks/tasks.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+    imports: [TasksModule, UsersModule],
     controllers: [AppController],
 })
 export class AppModule {}
