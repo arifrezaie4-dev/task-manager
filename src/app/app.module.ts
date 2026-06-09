f333b2ba8455b126dd622736de1021e381d39cf2
Arif Rezaie <arifrezaie4@gmail.com>
2026-06-10 01:01:30 +0430

    feat(auth): create auth module, service and controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { TasksModule } from 'src/tasks/tasks.module';
 import { UsersModule } from 'src/users/users.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-    imports: [TasksModule, UsersModule, PrismaModule],
+    imports: [TasksModule, UsersModule, PrismaModule, AuthModule],
     controllers: [AppController],
 })
 export class AppModule {}
