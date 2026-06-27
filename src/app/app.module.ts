5922ea586dceccf72b02b0c11bec1568a70d6d28
Arif Rezaie <arifrezaie4@gmail.com>
2026-06-27 20:38:43 +0430

    refactor: use ConfigService for application settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { TasksModule } from 'src/tasks/tasks.module';
-import { UsersModule } from 'src/users/users.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { AuthModule } from 'src/auth/auth.module';
-import { TeamsModule } from 'src/teams/teams.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { TasksModule } from "src/tasks/tasks.module";
+import { UsersModule } from "src/users/users.module";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthModule } from "src/auth/auth.module";
+import { TeamsModule } from "src/teams/teams.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [TasksModule, UsersModule, PrismaModule, AuthModule, TeamsModule],
-    controllers: [AppController],
+  imports: [
+    TasksModule,
+    UsersModule,
+    PrismaModule,
+    AuthModule,
+    TeamsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
