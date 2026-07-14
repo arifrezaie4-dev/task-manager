11e3f611cdb6be9b38cea298f75a7ad23632f9cf
Arif Rezaie <arifrezaie4@gmail.com>
2026-07-14 11:57:41 +0430

    refactor(frontend): use environment variable for API URL

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,1 +1,4 @@
-export const API_URL = "https://taskflow-backend-rh2p.onrender.com"
+// export const API_URL = "https://taskflow-backend-rh2p.onrender.com"
+export const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:3000";
